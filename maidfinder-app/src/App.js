9990a8988be09894fceb8461024ad67e0940e9fb
Abdul Samad <asp1979@hotmail.com>
2022-08-26 16:25:35 +0500

    initial 2
    initial 2

--- maidfinder-app/src/App.js
+++ maidfinder-app/src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import { Route } from 'react-router';
+import Signup from './components/Signup';
+import Profile from './components/Profile';
+import About from './components/About';
+
 
 function App() {
   return (
+    
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+<div>
+<h1>MaidFinder</h1>
+<hr></hr>
+
+dd
+
+
+</div>
+<div>
+Main
+</div>
     </div>
+    
   );
 }
 
