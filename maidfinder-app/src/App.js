d32a733c81dde3dc5d2448c2e143e4b541d6d01d
Abdul Samad <asp1979@hotmail.com>
2022-08-28 20:32:58 +0500

    updated

--- maidfinder-app/src/App.js
+++ maidfinder-app/src/App.js
@@ -1,27 +1,31 @@
 import './App.css';
-import { Route } from 'react-router';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Signup from './components/Signup';
 import Profile from './components/Profile';
 import About from './components/About';
+import Layout from "./components/Shared/Layout";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Home from './components/Home';
 
 
 function App() {
   return (
-    
     <div className="App">
+      hello2
 <div>
-<h1>MaidFinder</h1>
-<hr></hr>
-
-dd
-
-
-</div>
-<div>
-Main
+<BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="Profile" element={<Profile />} />
+          <Route path="About" element={<About />} />
+          <Route path="Signup" element={<Signup />} />
+          <Route path="*" element={<Home />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
 </div>
     </div>
-    
   );
 }
 
