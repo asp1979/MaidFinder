6b2d4d2c90f6e0600bdd5eb384e5e79975de4ef1
Abdul Samad <asp1979@hotmail.com>
2022-09-02 12:17:46 +0500

    started working on city
    started working on city

---
+++ MaidFinderWebAPI/MaidFinderWebAPI/Controllers/CityController.cs
@@ -0,0 +1,42 @@
+﻿using MaidFinderWebAPI.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace MaidFinderWebAPI.Controllers
+{
+    [Route("api/[controller]/[Action]")]
+    [ApiController]
+    public class CityController : ControllerBase
+    {
+        private readonly MaidFinderDbContext _context;
+
+        public CityController(MaidFinderDbContext context)
+        {
+            _context = context;
+        }
+
+
+        // GET: api/Customer
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<City>>> GetAllCity()
+        {
+            return await _context.City.ToListAsync();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Candidate>> CreateCandidate(City city)
+        {
+            _context.City.Add(city);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("success", new { id = city.CityID }, city);
+        }
+
+
+    }
+}
---
+++ MaidFinderWebAPI/MaidFinderWebAPI/Model/City.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MaidFinderWebAPI
+{
+    public class City
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public string CityID { get; set; }
+        [Column(TypeName = "nvarchar(100)")]
+        public string CityName { get; set; }
+
+    }
+}
